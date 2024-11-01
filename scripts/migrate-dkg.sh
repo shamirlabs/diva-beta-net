801853de818082d57d4225f95d543e12fb9a14b0
Kévin Berthommier <bertho-zero@users.noreply.github.com>
2024-11-01 18:05:01 +0100

    fix migrate-dkg

--- scripts/migrate-dkg.sh
+++ scripts/migrate-dkg.sh
@@ -4,18 +4,23 @@ exec_path=$1
 cd $exec_path
 
 set -a && source .env && set +a
-response=$(curl -s -X 'GET' \
+response=$(curl -sS -X 'GET' \
   'http://localhost:30000/api/v2/participant/dkgs' \
   -H 'accept: application/json' \
-  -H "Authorization: Bearer $DIVA_API_KEY")
+  -H "Authorization: Bearer $DIVA_API_KEY" 2>&1)
+
+if [ $? -ne 0 ]; then
+  echo "$response"
+  exit 1
+fi
 
 old_dkgs=$(echo "$response" | jq -r '. | if .error then 1 else 0 end')
 
 if [ "$old_dkgs" -eq 1 ]; then
   code=$(echo "$response" | jq -r '.error.code')
-  
+
   if [ "$code" == "PARTICIPANT_DELETING_ALL_DKGS" ]; then
-    curl -s -X 'DELETE' \
+    curl -sS -X 'DELETE' \
       'http://localhost:30000/api/v2/participant/dkgs' \
       -H 'accept: application/json' \
       -H "Authorization: Bearer $DIVA_API_KEY"
