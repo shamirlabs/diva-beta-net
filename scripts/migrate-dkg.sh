d596348e9022a79641c7c98d59e81fc322c87e37
root <franjoespejo@gmail.com>
2024-11-27 17:38:20 +0100

    beta 5
    beta 5
    
    beta 5

--- scripts/migrate-dkg.sh
+++ scripts/migrate-dkg.sh
@@ -7,12 +7,12 @@ if ! command -v jq &> /dev/null; then
   echo "Installing jq..."
   sudo apt-get update && sudo apt-get install -y jq
 fi
-
+docker compose up -d
 set -a && source .env && set +a
 response=""
 while [ -z "$response" ]; do
-  response=$(curl -s -X 'GET' \
-    'http://localhost:30000/api/v2/participant/dkgs' \
+  response=$(curl -s -X 'POST' \
+    'http://localhost:30000/api/v2/node/reset' \
     -H 'accept: application/json' \
     -H "Authorization: Bearer $DIVA_API_KEY")
   
@@ -20,16 +20,3 @@ while [ -z "$response" ]; do
     sleep 5
   fi
 done
-
-old_dkgs=$(echo "$response" | jq -r '. | if .error then 1 else 0 end')
-
-if [ "$old_dkgs" -eq 1 ]; then
-  code=$(echo "$response" | jq -r '.error.code')
-  
-  if [ "$code" == "PARTICIPANT_DELETING_ALL_DKGS" ]; then
-    curl -s -X 'DELETE' \
-      'http://localhost:30000/api/v2/participant/dkgs' \
-      -H 'accept: application/json' \
-      -H "Authorization: Bearer $DIVA_API_KEY"
-  fi
-fi
